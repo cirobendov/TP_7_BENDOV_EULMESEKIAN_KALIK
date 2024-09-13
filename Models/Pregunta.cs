d72f62135f4e52a40618b2a9c92abe7735919253
Pablo Ben Dov <cococartera@air-de-pablo.home>
2024-09-13 20:37:04 -0300

    m

--- Models/Pregunta.cs
+++ Models/Pregunta.cs
@@ -1,7 +1,7 @@
 public class Pregunta
 {
-    public static int Posicion{get; set;}
-    public static int IdPregunta{get; set;}
+    public int pos{ get; set; }
+    public int IdPregunta{get; set;}
     public int IdCategoria{get; set;}
     public int IdDificultad{get; set;}
     public string? Enunciado{get; set;}
